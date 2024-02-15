3af4626248ef762738c54299887c19b74251e426
Valentin Heiserer <valentin@heiserer.de>
2024-02-15 21:14:39 +0100

    test_sprite

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,14 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
-        .add_plugins((DefaultPlugins, HelloPlugin))
+        .add_plugins((DefaultPlugins.set(
+            // This sets image filtering to nearest
+            // This is done to prevent textures with low resolution (e.g. pixel art) from being blurred
+            // by linear filtering.
+            ImagePlugin::default_nearest(),
+        ), HelloPlugin))
+        .add_systems(Startup, setup)
+        .add_systems(Update, sprite_movement)
         .run();
 }
 
@@ -52,5 +59,56 @@ impl Plugin for HelloPlugin {
         app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
             .add_systems(Startup, add_people)
             .add_systems(Update, ((update_people, greet_people).chain()));
+    }
+}
+
+#[derive(Component)]
+enum DirectionX {
+    Left,
+    Right,
+}
+
+#[derive(Component)]
+enum DirectionY {
+    Up,
+    Down,
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn((
+        SpriteBundle {
+            texture: asset_server.load("pixel/bevy_pixel_dark.png"),
+            transform: Transform::from_xyz(100., 0., 0.),
+            ..default()
+        },
+        DirectionX::Right,
+        DirectionY::Up,
+    ));
+}
+
+fn sprite_movement(time: Res<Time>, mut sprite_position: Query<(&mut DirectionX, &mut DirectionY, &mut Transform)>) {
+    for (mut logo, mut logo2, mut transform) in &mut sprite_position {
+        match *logo {
+            DirectionX::Right => transform.translation.x += 30. * time.delta_seconds(),
+            DirectionX::Left => transform.translation.x -= 30. * time.delta_seconds(),
+        }
+        
+        if transform.translation.x > 200. {
+            *logo = DirectionX::Left;
+        } else if transform.translation.x < -200. {
+            *logo = DirectionX::Right;
+        }
+
+        match *logo2 {
+            DirectionY::Up => transform.translation.y += 10. * time.delta_seconds(),
+            DirectionY::Down => transform.translation.y -= 10. * time.delta_seconds(),
+        }
+        
+        if transform.translation.y > 200. {
+            *logo2 = DirectionY::Down;
+        } else if transform.translation.y < -200. {
+            *logo2 = DirectionY::Up;
+        }
     }
 }
